c24432bd559ab03fa868ea54f6d2cb77b60902a7
vivesal1 <91783441+vivesal1@users.noreply.github.com>
2022-09-13 11:50:45 +0200

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal)
+streamlit.text('Omega 3 & Blueberry Oatmeal')
